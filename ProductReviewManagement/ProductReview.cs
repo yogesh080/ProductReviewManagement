6a7690bf250e66e35bd503db5e5d4108525acea5
Yogesh <yogeshbietjhs@gmail.com>
2022-07-26 01:44:07 +0530

    product added

--- ProductReviewManagement/ProductReview.cs
+++ ProductReviewManagement/ProductReview.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ProductReviewManagement
 {
-    internal class ProductReview
+    public class ProductReview
     {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
     }
 }
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace ProductReviewManagement
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ProductReviewManagement
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ ProductID =1,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ ProductID =2,UserID=1,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ ProductID =3,UserID=2,Rating=3,Review="Good",isLike=true},
+                new ProductReview(){ ProductID =4,UserID=2,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ ProductID =5,UserID=3,Rating=1,Review="bad",isLike=true},
+                new ProductReview(){ ProductID =6,UserID=4,Rating=3,Review="nice",isLike=true},
+                new ProductReview(){ ProductID =7,UserID=4,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ ProductID =8,UserID=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ ProductID =9,UserID=10,Rating=2,Review="bad",isLike=true},
+                new ProductReview(){ ProductID =10,UserID=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ ProductID =11,UserID=10,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ ProductID =12,UserID=10,Rating=4,Review="nice",isLike=true},
+                new ProductReview(){ ProductID =13,UserID=10,Rating=2,Review="Good",isLike=true},
+                new ProductReview(){ ProductID =14,UserID=10,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ ProductID =15,UserID=10,Rating=1,Review="bad",isLike=true}
+
+
+            };
         }
     }
 }
